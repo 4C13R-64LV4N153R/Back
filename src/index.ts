38cdc3a46b7870c66a0aa3368d66cb42a5d3eee0
erazor <52050311+erazoor@users.noreply.github.com>
2024-07-05 00:04:45 +0200

    Revert "handle du token depuis le local storage"
    This reverts commit 06b6b6357cae05683c17d1f4012f19c6ee95e9be.

--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,34 @@
-import express from "express";
-import dotenv from "dotenv";
-import userRoutes from "./routes/userRoutes";
-import barRoutes from "./routes/BarRoutes";
-import livraisonRoutes from "./routes/livraisonRoutes";
-import authRoutes from "./routes/authRoutes";
-import adminRoutes from "./routes/adminRoutes";
-import { setupSwagger } from "./swagger";
-import cors from "cors";
+import express from 'express';
+import dotenv from 'dotenv';
+import userRoutes from './routes/userRoutes';
+import barRoutes from './routes/barRoutes';
+import livraisonRoutes from './routes/livraisonRoutes';
+import authRoutes from './routes/authRoutes';
+import adminRoutes from './routes/adminRoutes';
+import { setupSwagger } from './swagger';
+import cors from 'cors';
 
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 app.use(express.json());
 app.use(cors());
 
-app.use("/users", userRoutes);
-app.use("/auth", authRoutes);
-app.use("/bars", barRoutes);
-app.use("/livraisons", livraisonRoutes);
-app.use("/admin", adminRoutes);
+app.use('/users', userRoutes);
+app.use('/auth', authRoutes);
+app.use('/bars', barRoutes);
+app.use('/livraisons', livraisonRoutes);
+app.use('/admin', adminRoutes);
 
 setupSwagger(app);
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+    console.log(`Server is running on port ${port}`);
 });
